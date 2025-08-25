f8acb46974893581879e16d024eb4403adc36717
Javier Orlando Mantilla Portilla <javiermantilla@seti.com.co>
2025-08-25 16:18:46 -0500

    Se sube construccion de imagen para registry de webflux

--- fibonacci-webflux/build_and_push.sh
+++ fibonacci-webflux/build_and_push.sh
@@ -3,12 +3,12 @@
 mvn clean package -DskipTests
 
 # 2. Construir la imagen
-docker build -t fibonacci-mvc:latest .
+docker build -t fibonacci-webflux:latest .
 
 # 3. Etiquetar y subir al registry local
-docker tag fibonacci-mvc:latest 192.168.1.6:5000/fibonacci-mvc:latest
+docker tag fibonacci-mvc:latest 192.168.1.6:5000/fibonacci-webflux:latest
 # save en local-regitry la imagen--> image: registry:2
-docker push 192.168.1.6:5000/fibonacci-mvc:latest
+docker push 192.168.1.6:5000/fibonacci-webflux:latest
 
 #chmod +x build_and_push.sh
 #./build_and_push.sh
