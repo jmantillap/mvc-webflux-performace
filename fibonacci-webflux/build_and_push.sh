539cff7f0cb8b93756397c37307b2d7a40c6bbec
Javier Orlando Mantilla Portilla <javiermantilla@seti.com.co>
2025-08-25 17:07:56 -0500

    Se realiza ajustes a los archivos

--- fibonacci-webflux/build_and_push.sh
+++ fibonacci-webflux/build_and_push.sh
@@ -6,7 +6,7 @@ mvn clean package -DskipTests
 docker build -t fibonacci-webflux:latest .
 
 # 3. Etiquetar y subir al registry local
-docker tag fibonacci-mvc:latest 192.168.1.6:5000/fibonacci-webflux:latest
+docker tag fibonacci-webflux:latest 192.168.1.6:5000/fibonacci-webflux:latest
 # save en local-regitry la imagen--> image: registry:2
 docker push 192.168.1.6:5000/fibonacci-webflux:latest
 
