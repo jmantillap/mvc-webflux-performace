2243684c054fea9ec808231ffe1b0d88d9dbe469
Javier Orlando Mantilla Portilla <javiermantilla@seti.com.co>
2025-08-25 14:26:01 -0500

    Se agrega funcionaidad de poder compilar y subir a un registry local

---
+++ fibonacci-mvc/build_and_push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 1. Compilar con Maven
+mvn clean package -DskipTests
+
+# 2. Construir la imagen
+docker build -t fibonacci-mvc:latest .
+
+# 3. Etiquetar y subir al registry local
+docker tag fibonacci-mvc:latest 192.168.1.6:5000/fibonacci-mvc:latest
+# save en local-regitry la imagen--> image: registry:2
+docker push 192.168.1.6:5000/fibonacci-mvc:latest
